47df213ac8f0cc9c7ccc170be9082ac51d4eb2be
sadio-kanoute <sadio.kanoute@laplateforme.io>
2025-08-05 16:03:46 +0200

    modifcation bdd configurer pour metre en ligne sur plesk

--- bdd.php
+++ bdd.php
@@ -5,7 +5,7 @@ if (session_status() === PHP_SESSION_NONE) {
 }
 
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=moduleconnexion;charset=utf8', 'root', '');
+    $bdd = new PDO('mysql:host=localhost;dbname=sadio-kanoute_moduleconnexion;charset=utf8', 'sadio-kanoute', 'Adama@1974@');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
     die("Erreur BDD : " . $e->getMessage());
